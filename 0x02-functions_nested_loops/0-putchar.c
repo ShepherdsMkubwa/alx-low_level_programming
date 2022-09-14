ef87d79c34dc41e2843f62951fc433d1183db1cf
ShepherdsMkubwa <muriuki.timothymuriithi@gmail.com>
2022-09-14 05:24:26 -0700

    alphabet 10x

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,16 +1,16 @@
 #include <unistd.h>
-#include <main.h>
+#include "main.h"
 /**
  * main - Check description
- * Description: It prints the word Holberton, followed by a new line.
+ * Description: It prints the word _putchar, followed by a new line.
  * Return: 0.
  */
-int main(void) 
-{  
-char word[9] = "Holberton";  
-int i;  
+int main(void)
+{
+char word[9] = "Holberton";
+int i;
 for (i = 0; i < 9; i++)
-_putchar(word[i]); 
+_putchar(word[i]);
 _putchar('\n');
-return (0); 
+return (0);
 }
