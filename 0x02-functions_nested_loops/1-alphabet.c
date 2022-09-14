966b48336df99ca9fc90b876b2a70b934a73c72e
ShepherdsMkubwa <muriuki.timothymuriithi@gmail.com>
2022-09-14 05:07:07 -0700

    betty corrections

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -4,10 +4,10 @@
  * Description: It prints the alphabet in lowercase fallowed by a new line
  * Return: Nothing.
  */
-void print_alphabet(void) 
-{  
+void print_alphabet(void)
+{ 
 char i;
-for (i = 'a'; i <= 'z'; i++)    
+for (i = 'a'; i <= 'z'; i++)   
 _putchar(i);
-_putchar('\n'); 
+_putchar('\n');
 }
